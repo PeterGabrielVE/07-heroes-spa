990a5baaf4e6a5ad51c34247dd68dfdaddcda6c3
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2022-09-08 22:05:58 -0400

    useNavigate

--- src/auth/Pages/LoginPage.jsx
+++ src/auth/Pages/LoginPage.jsx
@@ -1,5 +1,24 @@
+import { Link, NavLink, useNavigate } from 'react-router-dom';
+
 export const LoginPage = () => {
+
+  const navigate = useNavigate();
+
+    const onLogin = () =>{
+        navigate('/',{
+            replace: true
+        });
+        
+  }
+
   return (
-    <h1>LoginPage</h1>
+    <div>
+       <h1>LoginPage</h1>
+       <hr />
+       <button className="btn btn-primary" onClick={ onLogin }>
+          Login
+       </button>
+    </div>
+   
   )
 }
