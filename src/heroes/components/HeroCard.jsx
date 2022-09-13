fc32ec3d2400103f00d5e75082b1845729c1049d
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2022-09-13 11:11:44 -0400

    tarjeta de heroe parte2

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,4 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
+
+const CharactesByHero = ({ alter_ego, characters }) =>{
+
+    if( alter_ego === characters ) return (<></>);
+
+    return <p>{ characters }</p>
+}
+
 
 export const HeroCard = ({
     id,
@@ -10,17 +20,32 @@ export const HeroCard = ({
 }) => {
 
     const heroImageUrl = `/assets/heroes/${ id }.jpg`;
+
+    //const charactesByHero = (<p>{ characters }</p>);
   return (
     <div className='col'>
         <div className='card'>
             <div className='row no-gutters'>
                 <div className='col-4'>
                     <img src={ heroImageUrl } className="card-img" alt="" />
+                </div>
+                <div className='col-8'>
                     <div className="card-body">
                         <h5 className="card-title">{ superhero }</h5>
                         <p className="card-text">{ alter_ego }</p>
-                    </div>
+                        {/*
+                            ( alter_ego !== characters ) && charactesByHero
+                            */
+                        }
+                        <CharactesByHero characters={ characters } alter_ego={ alter_ego } />
+                        <p className='card-text'>
+                            <small className='text-muted'>{ first_appearance }</small>
+                        </p>
 
+                        <Link to={`/hero/${ id }`}>
+                            Más...
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
