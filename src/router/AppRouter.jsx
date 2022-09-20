2cb5443d726709bf5ef83a85b28368bbf453d0ed
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2022-09-19 20:40:23 -0400

    RoutePublic

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -9,7 +9,18 @@ export const AppRouter = () => {
   return (
     <>
         <Routes>
-            <Route path='login' element={ <LoginPage/>} />
+            
+            
+            <Route path="login/*" element={
+                <PublicRoute>
+                  {/* <LoginPage /> */}
+                  <Routes>
+                    <Route path="/*" element={<LoginPage />} />
+                  </Routes>
+                </PublicRoute>
+              }
+            />
+            
             
             <Route path="/*" element={ <PrivateRoute>
               <HeroesRoutes />
