1312f58640ce25ba4ac42461e2f7553036e476ab
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2022-09-19 20:31:46 -0400

    public route y public

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,13 +3,18 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import { Navbar } from '../ui/components/Navbar'
 import { LoginPage } from '../auth'
 import { HeroesRoutes } from '../heroes'
+import { PrivateRoute } from './PrivateRoute'
 
 export const AppRouter = () => {
   return (
     <>
         <Routes>
             <Route path='login' element={ <LoginPage/>} />
-            <Route path="/*" element={<HeroesRoutes />} />
+            
+            <Route path="/*" element={ <PrivateRoute>
+              <HeroesRoutes />
+            </PrivateRoute> } />
+           
            
         </Routes>
     </>
